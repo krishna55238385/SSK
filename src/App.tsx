b92051a4fd00c112d9d3ba9c9923614585d3b153
Arjunmb01 <arjunmb1176@gmail.com>
2026-08-02 14:17:32 +0530

    Fix GSAP scroll pinning on home page by clearing page transitions transform

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,15 @@ const PageWrapper: React.FC<PageWrapperProps> = ({ children }) => {
     gsap.fromTo(
       ref.current,
       { opacity: 0, y: 20 },
-      { opacity: 1, y: 0, duration: 0.6, ease: 'power2.out' }
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        ease: 'power2.out',
+        onComplete: () => {
+          gsap.set(ref.current, { clearProps: "transform,translate,y" })
+        }
+      }
     )
   }, [location.pathname])
 
